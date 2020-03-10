8f8294eee685edc2164e4f12cd8d558c6820acba
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-03-10 21:05:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,10 @@
 class Triangle
-  # write code here
+  
+  def initialize(side1, side2, side3)
+    @triangle_sides = []
+    @triangle_sides << side1
+    @triangle_sides << side2
+    @triangle_sides << side3
+  end
+  
 end
